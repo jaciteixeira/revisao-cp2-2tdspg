211cb9cf40650e9ce4842bde16d3a993ba7d5d11
Benefrancis <benefrancis@gmail.com>
2024-04-12 10:34:06 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/revisaocp22tdspg/RevisaoCp22tdspgApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.revisaocp22tdspg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisaoCp22tdspgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( RevisaoCp22tdspgApplication.class, args );
+    }
+
+}
---
+++ src/test/java/br/com/fiap/revisaocp22tdspg/RevisaoCp22tdspgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.revisaocp22tdspg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisaoCp22tdspgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
